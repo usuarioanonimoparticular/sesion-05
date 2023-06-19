42a22aeb1feb13660adf8bcc0a24adb8fbbdb518
Kevin Romero <108586694+usuarioanonimoparticular@users.noreply.github.com>
2023-06-19 18:21:45 -0500

    Actualizar 1er_ejercicio.cpp

--- 1er_ejercicio.cpp
+++ 1er_ejercicio.cpp
@@ -53,3 +53,43 @@ int main()
     a.info();
     return 0;
 }
+
+
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    cout << "Inicio" << endl;
+    try {
+        cout << "Dentro del bloque try" << endl;
+        throw("5.5"); // char const*
+        // throw(5);  int
+        // throw(5.5);  double
+        // throw(5.5f);  float
+        cout << "NO SALE" << endl;
+    }
+    catch (int i) {
+        cout << "Capturando entero: " << endl;
+        cout << i << endl;
+    }
+    catch (double d) {
+        cout << "Capturando double: " << endl;
+        cout << d << endl;
+    }
+    catch (float f) {
+        cout << "Capturando float: " << endl;
+        cout << f << endl;
+    }
+    catch (char const* c) {
+        cout << "Capturando string: " << endl;
+        cout << c << endl;
+    }
+    cout << "Fin del programa" << endl;
+    system("pause");
+    system("cls");
+    return 0;
+}
