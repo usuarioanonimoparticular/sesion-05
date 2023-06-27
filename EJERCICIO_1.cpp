836de273dc7b2dc53e959e409c034d1aa8f8e283
Kevin Romero <108586694+usuarioanonimoparticular@users.noreply.github.com>
2023-06-27 09:24:47 -0500

    Actualizar EJERCICIO_1.cpp

--- EJERCICIO_1.cpp
+++ EJERCICIO_1.cpp
@@ -21,3 +21,8 @@ class veiculo
         }
         veiculo(int a, int b, int c, int d)
 }
+
+// https://www.scimagojr.com/journalsearch.php?q=21100450131&tip=sid&clean=0
+https://www.scimagojr.com/journalsearch.php?q=21100898028&tip=sid&clean=0
+https://www.scimagojr.com/journalsearch.php?q=5300152203&tip=sid&clean=0
+https://www.scimagojr.com/journalsearch.php?q=21100898825&tip=sid&clean=0
